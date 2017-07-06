33a3c5517c96be2358cbb93ddf9e96c71cf52fe9
khai-dv <khai-dv@fujinet.vn>
2017-07-06 10:08:55 +0700

    git add src/app/app.component.ts

--- Lesson1/src/app/app.component.ts
+++ Lesson1/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sample of khai-dv';
+  title = 'New sample of khai-dv';
 }
