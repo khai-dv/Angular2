54e4a7920c11827ce36a419da7da71e3d4d7e14c
khai-dv <khai-dv@fujinet.vn>
2017-07-14 10:35:58 +0700

    data binding

--- Lesson1/src/app/app.module.ts
+++ Lesson1/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PeopleListComponent } from './people-list.component';
 import { PeopleService } from './people.service';
+import { PeopleListComponent } from './people-list.component';
+import { PersonDetailsComponent } from './person-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleListComponent
+    PeopleListComponent, 
+    PersonDetailsComponent
   ],
   imports: [
     BrowserModule
--- Lesson1/src/app/people-list.component.ts
+++ Lesson1/src/app/people-list.component.ts
@@ -5,17 +5,27 @@ import { PeopleService } from './people.service';
 @Component({
   selector: 'people-list',
   template: `
-  <!-- this is the new syntax for ng-repeat -->
-  <ul>
-    <li *ngFor="let person of people">
-        {{person.name}}
-    </li>
-  </ul>
+    <!-- this is the new syntax for ng-repeat -->
+    <ul>
+        <li *ngFor="let person of people">
+        <a href="#" (click)="selectPerson(person)">
+            {{person.name}}
+        </a>
+        </li>
+    </ul>
+
+    <person-details [person]="selectedPerson"></person-details>
   `
 })
 export class PeopleListComponent {
   people: Person[] = [];
+  selectedPerson: Person;
+
   constructor(private _peopleService : PeopleService){
     this.people = _peopleService.getAll();
+  }
+
+selectPerson(person: Person){
+    this.selectedPerson = person;
   }
 }
